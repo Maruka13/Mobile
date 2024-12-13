48885ee7da2fbf2ec8124c5ac6d9b57abed550cd
Maruka <emanuelle_carollina@hotmail.com>
2024-12-13 08:39:33 -0300

    fontes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,23 @@
 import { Stack } from "expo-router"
 import { colors } from "@/styles/theme"
 
+import {
+    useFonts,
+    Rubik_600SemiBold,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold
+} from "@expo-google-fonts/rubik"
+
 export default function Layout() {
+
+    useFonts({
+        useFonts,
+    Rubik_600SemiBold,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold
+    })
 
     return (
         <Stack 
