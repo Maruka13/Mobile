057aab92f9f27c1513798e269d0424fa84546c07
Maruka <emanuelle_carollina@hotmail.com>
2024-12-13 08:46:03 -0300

    fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,14 +10,16 @@ import {
 } from "@expo-google-fonts/rubik"
 
 export default function Layout() {
-
-    useFonts({
-        useFonts,
-    Rubik_600SemiBold,
-    Rubik_400Regular,
-    Rubik_500Medium,
-    Rubik_700Bold
+    const [fontsLoaded] = useFonts({
+        Rubik_600SemiBold,
+        Rubik_400Regular,
+        Rubik_500Medium,
+        Rubik_700Bold
     })
+
+    if (!fontsLoaded) {
+        return
+    }
 
     return (
         <Stack 
