3137532c2850a981091116f267ccbc4cb5f153fe
Maruka <emanuelle_carollina@hotmail.com>
2024-12-18 21:14:18 -0300

    button

--- src/app/components/button/index.tsx
+++ src/app/components/button/index.tsx
@@ -3,16 +3,28 @@ import { TouchableOpacity, TouchableOpacityProps, Text, TextProps } from "react-
 import { s } from "./styles"
 import { colors } from "@/styles/theme"
 
+type ButtonProps = TouchableOpacityProps & {
+    isLoading?: boolean
+}
 
-function Button({ children }: TouchableOpacityProps) {
-    return <TouchableOpacity style={s.container} activeOpacity={0.8}>
-        {children}
+function Button({ children, style, isLoading = false, ...rest }: ButtonProps) {
+    return (
+    <TouchableOpacity 
+        style={[s.container, style]} 
+        activeOpacity={0.8} 
+        disabled={isLoading}>
+        {...rest}
+
+        {isLoading ? ( <ActivityIndicator size="small" color={colors.gray[100]} /> : ( children )}
         </TouchableOpacity>
+    )
 }
 
 function Title({ children }: TextProps) {
     return <Text style={s.title}>{children}</Text>
 }
+
+
 
 Button.Title = Title 
 
